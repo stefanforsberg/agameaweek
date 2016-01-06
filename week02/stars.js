a603530a38a5e2e9e3781acb3364c883a998e411
Stefan Forsberg <cykelkatalogen@popkram.com>
2016-01-06 21:38:06 +0100

    Color of stars

--- week02/stars.js
+++ week02/stars.js
@@ -24,13 +24,20 @@ function star() {
 	this.y = Math.random()*game.settings.height;
 	this.vx = -1/30 + Math.random() / 20;
 	this.vy = -1/30 +  Math.random() / 20;
-	this.radius = Math.random() * 2.5;
-	this.alpha = 0.02 + 0.3*Math.random();
+	this.radius = 1 + Math.random() * 1.5;
+	this.alpha = 0.02 + 0.25*Math.random();
+	this.setColor();
+}
+
+star.prototype.setColor = function() {
+	var colors = ["151, 170, 246", "235, 234, 240", "250, 206, 159"]
+	this.color = colors[Math.floor(Math.random()*colors.length)];
 }
 
 star.prototype.draw = function() {
+
 	game.context.beginPath();
-	game.context.fillStyle = "rgba(255,255,255," + this.alpha + ")";
+	game.context.fillStyle = "rgba(" + this.color + "," + this.alpha + ")";
 	game.context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
 	game.context.fill();
 
