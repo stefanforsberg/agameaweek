d033a0fa1d4ebcc84c1ff41217ec3f0a58c5acc2
Stefan Forsberg <cykelkatalogen@popkram.com>
2016-01-10 21:28:54 +0100

    Diamonds and birds

--- week03/game.js
+++ week03/game.js
@@ -16,7 +16,10 @@ game.start = function() {
 
 	game.speed = 1;
 
+	game.player.init();
 	game.terrain.init();
+	game.diamond.init();
+	game.bird.init();
 
 	game.loop = Rx.Observable.interval(1000/66).subscribe(function (t) {
 		game.update(t);
@@ -31,15 +34,16 @@ game.start = function() {
 		})
 		.scan(function (p, k) {
 			if(k === 37) {
-				if(p.x > 0) {
-					p.x--;
+				if(p.x > 32) {
+					p.x-= 64;
 				}
 			} else {
-				if(p.x < 4) {
-					p.x++;	
+				if(p.x < 288) {
+					p.x+= 64;	
 				}
 				
 			}
+
 			return p;
 		}, game.player)
 		.subscribe();
@@ -49,22 +53,87 @@ game.update = function(t) {
 	game.context.clearRect(0,0,game.settings.width,game.settings.height);
 	game.terrain.update(t);
 	game.player.update(t);
-	game.potion.update(t);
+	game.diamond.update(t);
+	game.bird.update(t);
 }
 
-game.potion = {
+game.bird = {
 	x: 1,
 	y: 15,
-	image: document.getElementById("mountain"),
+	width: 32,
+	height: 32,	
+	sprites: [document.getElementById("bird01"), document.getElementById("bird02"),document.getElementById("bird03"),document.getElementById("bird02")],
+	currentSprite: 0,	
+	
 	init: function() {
+		this.x = 48 + game.randomNumber(0,4)*64;
+		this.y = -game.randomNumber(10, 50);
+		this.vy = 1+Math.random();
+		this.xdeltaSize = 1 + 25*Math.random();
+		this.ydeltaSize = 1 + 5*Math.random();
 
+		console.log(this.xdeltaSize);
 	},
 	update: function(t) {
 
-		game.context.drawImage(this.image,game.settings.tileSize + game.settings.tileSize/2 + this.x*game.settings.tileSize*2, this.y);
+		if(game.collides(this,game.player)) {
+			this.init();
+		} else if(this.y > (game.settings.height + 20)) {
+			this.init();
+		}
 
-		this.y++;
+		var xdelta = this.xdeltaSize*Math.cos(Math.PI*t*2/180);
+		var ydelta = this.ydeltaSize*Math.sin(Math.PI*t*4/180);
 
+		game.context.drawImage(this.sprites[this.currentSprite],this.x + xdelta, this.y + ydelta);
+
+		if(t % 15 === 0) {
+			this.currentSprite++;
+			if(this.currentSprite > (this.sprites.length-1)) {
+				this.currentSprite = 0;
+			}	
+		}
+
+		this.y += this.vy;
+	}
+}
+
+game.diamond = {
+	x: 1,
+	y: 15,
+	width: 32,
+	height: 32,
+	sprites: [document.getElementById("diamond01"),document.getElementById("diamond02"),document.getElementById("diamond03"),document.getElementById("diamond04")],
+	currentSprite: 0,
+	init: function() {
+		this.x = 48 + game.randomNumber(0,4)*64;
+		this.y = 15;
+		this.vy = 1 + Math.random();
+	},
+	update: function(t) {
+
+		game.context.save();
+		game.context.translate(this.x + (this.width/2), this.y + (this.height/2));
+		game.context.rotate(t*Math.PI/180);		
+		game.context.drawImage(this.sprites[this.currentSprite],-(this.width/2), -(this.height/2), 24, 24);
+		game.context.restore();
+
+		
+
+		if(game.collides(this,game.player)) {
+			this.init();
+		} else if(this.y > (game.settings.height + 20)) {
+			this.init();
+		}
+
+		if(t % 30 === 0) {
+			this.currentSprite++;
+			if(this.currentSprite > (this.sprites.length-1)) {
+				this.currentSprite = 0;
+			}	
+		}
+
+		this.y += this.vy;
 	}
 }
 
@@ -97,8 +166,6 @@ game.terrain = {
 		}
 		
 		this.context.restore();
-
-		
 	},
 	update: function() {
 
@@ -116,25 +183,46 @@ game.terrain = {
 
 game.player = {
 	x: 2,
-	y: 12,
+	y: 384,
+	width: 64,
+	height: 64,
 	currentSprite: 0,
 	sprites: [document.getElementById("witch01"), document.getElementById("witch02")],
+	init: function() {
+		this.x = 160;
+		this.y = 384
+	},
 	update: function(t) {
 
 		var xdelta = 5*Math.cos(Math.PI*t*2/180);
 		var ydelta = 5*Math.sin(Math.PI*t*4/180);
 
 		game.context.save();
-		//game.context.imageSmoothingEnabled = false;
-		game.context.drawImage(this.sprites[this.currentSprite], game.settings.tileSize + this.x*game.settings.tileSize*2 + xdelta, this.y*game.settings.tileSize + ydelta, 64, 64);
+		game.context.imageSmoothingEnabled = false;
+		game.context.drawImage(this.sprites[this.currentSprite], this.x + xdelta, this.y + ydelta, 64, 64);
 		game.context.restore();
 
-		if(t % 10 === 0) {
+		if(t % 15 === 0) {
 			this.currentSprite++;
 			if(this.currentSprite > (this.sprites.length-1)) {
 				this.currentSprite = 0;
 			}	
 		}
-		
 	}
+}
+
+game.collides = function colCheck(shapeA, shapeB) {
+    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2));
+    var vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2));
+    var hWidths = (shapeA.width / 2) + (shapeB.width / 2);
+    var hHeights = (shapeA.height / 2) + (shapeB.height / 2);
+
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+        return true;
+    }
+    return false;
+};
+
+game.randomNumber = function(low, high) {
+	return Math.floor(Math.random()*(1+high-low))+low;
 }
